1e16fba0a862278f6dc1ac1ecb7abf2c4af73990
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-04-13 09:46:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+Bot=Client(
+  "pyrogram bot",
+  bot_token="2111463729:AAFYsibwDapnq2gUplJQLeNKHNQvwUGwGoI",
+  api_id=17114587,
+  api_hash="b1c07d33747425d84050b68bae6be91f"
+  
+)
+
+@Bot.on_message(filters.command("start"))
+async def start_message(bot, message):
+  await message.reply("hi")
+  
+  
+Bot.run()

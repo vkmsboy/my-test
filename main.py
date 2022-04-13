b6adbf68ccb595b141a9b1b7d32cf51b3daa3c91
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-04-13 09:50:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ Bot=Client(
   bot_token="2111463729:AAFYsibwDapnq2gUplJQLeNKHNQvwUGwGoI",
   api_id=17114587,
   api_hash="b1c07d33747425d84050b68bae6be91f"
-  
-)
+  )
 
 @Bot.on_message(filters.command("start"))
 async def start_message(bot, message):
